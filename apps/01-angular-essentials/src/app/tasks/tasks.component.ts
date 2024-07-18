228fd9ecf3d3266239c97f9eecdfa7d7f759f87c
Helekin <clsjs94@hotmail.com>
2024-07-18 18:15:59 -0500

    new task

--- apps/01-angular-essentials/src/app/tasks/tasks.component.ts
+++ apps/01-angular-essentials/src/app/tasks/tasks.component.ts
@@ -1,13 +1,34 @@
 import { Component, Input } from '@angular/core';
+
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
-  selector: 'app-task',
+  selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTask = false;
+  // tasks:[]
+
+  // get selectedUserTasks(){
+  //   return this.tasks.filter(task=>task.userId===this.userId)
+  // }
+
+  onCompleteTask(id: string) {
+    // this.tasks=this.tasks.filter(task=>task.id!==id)
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
 }
