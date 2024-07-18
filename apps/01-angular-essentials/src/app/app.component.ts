60b6ac4a058800f35cfb6da8d173f7c68b44d74c
Helekin <clsjs94@hotmail.com>
2024-07-17 20:30:47 -0500

    tasks

--- apps/01-angular-essentials/src/app/app.component.ts
+++ apps/01-angular-essentials/src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from "./task/task.component";
+
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
