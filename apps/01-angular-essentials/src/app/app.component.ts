344c7e86bc2a6b75c7bf0f9b4589fed43a5ba0c7
Helekin <clsjs94@hotmail.com>
2024-07-18 16:22:13 -0500

    new task

--- apps/01-angular-essentials/src/app/app.component.ts
+++ apps/01-angular-essentials/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from "./task/task.component";
+import { TasksComponent } from './tasks/tasks.component';
 
 import { DUMMY_USERS } from './dummy-users';
 
@@ -15,4 +15,13 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectedUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
--- apps/01-angular-essentials/src/app/tasks/tasks.component.ts
+++ apps/01-angular-essentials/src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
-  templateUrl: './task.component.html',
-  styleUrl: './task.component.css',
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input() name?: string;
