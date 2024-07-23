a80e8c8e727309156218880fd7fee5c1c5c5038b
Helekin <clsjs94@hotmail.com>
2024-07-22 20:19:42 -0500

    final commit

--- apps/01-angular-essentials/src/app/tasks/tasks.component.ts
+++ apps/01-angular-essentials/src/app/tasks/tasks.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -14,21 +16,18 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  // tasks:[]
 
-  // get selectedUserTasks(){
-  //   return this.tasks.filter(task=>task.userId===this.userId)
-  // }
+  constructor(private tasksService: TasksService) {}
 
-  onCompleteTask(id: string) {
-    // this.tasks=this.tasks.filter(task=>task.id!==id)
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
