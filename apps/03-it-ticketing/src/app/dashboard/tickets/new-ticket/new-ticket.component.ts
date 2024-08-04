c9aad275f49bfb84ebfb4dde7bf215aeace106a7
Helekin <clsjs94@hotmail.com>
2024-08-03 20:47:14 -0500

    final commit: IT Ticketing

--- apps/03-it-ticketing/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ apps/03-it-ticketing/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, output, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ControlComponent } from '../../../shared/control/control.component';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
+  host: {
+    class: 'control',
+  },
 })
 export class NewTicketComponent {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  add = output<{ title: string; text: string }>();
 
+  ngOnInit() {
+    console.log('ONINIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  onSubmit(title: string, ticketText: string) {
+    this.add.emit({ title: title, text: ticketText });
+    this.form?.nativeElement.reset();
+  }
 }
