f02087634ca88432c7a9479ac8eb8031b54cc991
Helekin <clsjs94@hotmail.com>
2024-08-06 17:52:58 -0500

    new project #10

--- apps/09-place-picker-frontend/src/app/places/places.service.ts
+++ apps/09-place-picker-frontend/src/app/places/places.service.ts
@@ -3,6 +3,7 @@ import { catchError, map, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 import { Place } from './place.model';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,7 @@ import { Place } from './place.model';
 export class PlacesService {
   private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
+  private errorService = inject(ErrorService);
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
@@ -47,12 +49,31 @@ export class PlacesService {
       .pipe(
         catchError((error) => {
           this.userPlaces.set(prevPlaces);
+          this.errorService.showError('Failed to store selected place.');
           return throwError(() => new Error('Failed to store selected place.'));
         })
       );
   }
 
-  removeUserPlace(place: Place) {}
+  removeUserPlace(place: Place) {
+    const prevPlaces = this.userPlaces();
+
+    if (prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set(prevPlaces.filter((p) => p.id !== place.id));
+    }
+
+    return this.httpClient
+      .delete('http://localhost:3000/user-places/' + place.id)
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);
+          this.errorService.showError('Failed to remove the selected place.');
+          return throwError(
+            () => new Error('Failed to remove the selected place.')
+          );
+        })
+      );
+  }
 
   private fetchPlaces(url: string, errorMessage: string) {
     return this.httpClient.get<{ places: Place[] }>(url).pipe(
---
+++ apps/10-fake-register/README.md
@@ -0,0 +1,3 @@
+## About This Exercise
+
+This comprehensive exercise set covers section 13 of the Udemy course "Angular - The Complete Guide (2024 Edition)" instructed by Maximilian Schwarzmüller. It explores how to handling user Input and working with forms (Template-driven and Reactive).
